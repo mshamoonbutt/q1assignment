829acde9ceb1b8e24b535ed95f7b7462fdf0f5b7
mshamoonbutt <139213392+mshamoonbutt@users.noreply.github.com>
2023-08-26 16:08:41 +0500

    Delete Exercise 9 directory

--- Exercise 9/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
-let num = 2;
-let message = `My favorite number is ${num}`;
-console.log(message);
--- Exercise 9/index.ts
+++
@@ -1,4 +0,0 @@
-//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
-let num:number=2
-let message:string=`My favorite number is ${num}`
-console.log(message)
