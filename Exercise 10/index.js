a76a49a243390ede959f5f88ca9ea799b3cb8cb4
mshamoonbutt <139213392+mshamoonbutt@users.noreply.github.com>
2023-08-26 16:01:07 +0500

    Add files via upload

---
+++ Exercise 10/index.js
@@ -0,0 +1,13 @@
+"use strict";
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//My name is Shamoon
+//26/08/2023
+//This program shows a person name and a message
+let personName = "Shamoon";
+let message = `My name is ${personName}, I am student of PIAIC batch 47.`;
+console.log(message);
+//This program shows sum, difference, product and quotient
+console.log(6 + 2);
+console.log(13 - 5);
+console.log(4 * 2);
+console.log(24 / 3);
