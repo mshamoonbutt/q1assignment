996823b477f68347e61705ae27c7d90566e3bcc9
mshamoonbutt <139213392+mshamoonbutt@users.noreply.github.com>
2023-09-03 10:16:38 +0500

    Add files via upload

---
+++ Exercise 12/index.ts
@@ -0,0 +1,6 @@
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let names: string[]=['Ali','Bilal','Faizan','Haseeb'];
+for (let name of names)
+{
+    console.log(`How are you ${name}? Hope you are fine.`)
+}
